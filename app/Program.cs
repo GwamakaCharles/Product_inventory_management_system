921b9bc2995fcf06a7885a8fbf359b5add040191
Gwamaka Charles <gwaxart19@gmail.com>
2019-08-07 12:59:43 +0300

    My First commit.

---
+++ app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = {1,2,3,4,5,6,7};
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
